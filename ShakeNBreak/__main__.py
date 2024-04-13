ec15480e2c60c16c98445c791a2636cc92dd4c99
Dominick Filonowich <filonowichd74@gmail.com>
2024-04-12 23:54:09 -0400

    Fixed input errors

--- ShakeNBreak/__main__.py
+++ ShakeNBreak/__main__.py
@@ -12,8 +12,10 @@ def main():
     iParams = {} # Input Parameters
 
     # Get input file
-    iParams['structureFile'] = input('\nProvide path to input file: ')
-    iParams['length'] = input('\nPlease provide the number of atoms per molecue: ')
+    print('\nProvide path to input file: ')
+    iParams['structureFile'] = input()
+    print('\nPlease provide the number of atoms per molecue: ')
+    iParams['length'] = input()
 
     struc, unitCell = tm.readStrucFile(
         iParams['structureFile']) # Converts pdb or xyz files in pandas dataframe
@@ -25,38 +27,38 @@ def main():
         struc, iParams['length']) # Converts pandas dataframe to a list of lists
 
     # Choose whether or Shake or Break the system
-    iParams['mode'] = input(
-        '\nWhat would you like to do? (Shake, Break, ShakeNBreak, BreakNShake): ')
+    print('\nWhat would you like to do? (Shake, Break, ShakeNBreak, BreakNShake): ')
+    iParams['mode'] = input()
 
     # Run appropriate method for defined mode
     if iParams['mode'].lower() == 'shake':
-        iParams['magnitude'] = input(
-            '\nHow much would you like molecules to move? (Units in Angstroms): ')
+        print('\nHow much would you like molecules to move? (Units in Angstroms): ')
+        iParams['magnitude'] = input()
         outStruc, strucType = shakeMol(originalCoords, iParams) # Shake the structure
 
     elif iParams['mode'].lower() == 'break':
-        iParams['remove'] = input(
-            '\nWhat percent of molecules should be removed? (i.e. 10): ')
+        print('\nWhat percent of molecules should be removed? (i.e. 10): ')
+        iParams['remove'] = input()
         outStruc, strucType = breakMol(originalCoords, iParams) # Remove molecules from the structre
 
     elif iParams['mode'].lower() == 'shakenbreak':
-        iParams['magnitude'] = input(
-            '\nHow much would you like molecules to move? (Units in Angstroms): ')
+        print('\nHow much would you like molecules to move? (Units in Angstroms): ')
+        iParams['magnitude'] = input()
         midStruc, strucType = shakeMol(originalCoords, iParams) # Shake the structure
-        iParams['remove'] = input(
-            '\nWhat percent of molecules should be removed? (i.e. 10): ')
+        print('\nWhat percent of molecules should be removed? (i.e. 10): ')
+        iParams['remove'] = input()
         outStruc, strucType = breakMol(midStruc, iParams) # Remove molecules from the structre
 
     elif iParams['mode'].lower() == 'breaknshake':
-        iParams['remove'] = input(
-            '\nWhat percent of molecules should be removed? (i.e. 10): ')
+        print('\nWhat percent of molecules should be removed? (i.e. 10): ')
+        iParams['remove'] = input()
         midStruc, strucType = breakMol(originalCoords, iParams) # Remove molecules from the structre
-        iParams['magnitude'] = input(
-            '\nHow much would you like molecules to move? (Units in Angstroms): ')
+        print('\nHow much would you like molecules to move? (Units in Angstroms): ')
+        iParams['magnitude'] = input()
         outStruc, strucType = shakeMol(midStruc, iParams) # Shake the structure
 
-    iParams['output'] = input(
-        'Where would you like to save your file?: ') # provide path for outputfile to be written
+    print('Where would you like to save your file?: ')
+    iParams['output'] = input() # provide path for outputfile to be written
 
     if unitCell:
         tm.writeOutput(outStruc, iParams['output'], strucType, unitCell)
